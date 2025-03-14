4dfd027d6ae03d0cd2343879ff214d5017249a73
João Carlos Formento <jcformento13@gmail.com>
2025-03-14 10:40:02 -0300

    Movimentação e colisão da bola implementada

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
-
 const tecla_pressionada = {"KeyA": false, "KeyD": false}
+let game_over = false;
 
 document.addEventListener('keydown', (e) =>{
     switch(e.code) {
@@ -23,6 +23,9 @@ document.addEventListener('keyup', (e) =>{
             break
     }
 })
+//eventListeners para que quando o usuário pressione o botão a plataforma ande
+//e quando pare de pressionar ela pare.
+
 
 class Objetos {
     constructor(x, y, largura, altura){
@@ -35,13 +38,20 @@ class Objetos {
         ctx.fillStyle = cor
         ctx.fillRect(this.x, this.y, this.largura, this.altura)
     }
+    atualizar(){
+        throw new Error('Implementar nas classes filhas');
+    }
 }
 
 class Plataforma extends Objetos{
     #velocidade
+    #andarEsquerda
+    #andarDireita
     constructor(x, y, largura, altura){
         super(x, y, largura, altura);
         this.#velocidade = 0;
+        this.#andarDireita = 5;
+        this.#andarEsquerda = -5
     }
     andar(valorDirecao){
         this.#velocidade = valorDirecao;
@@ -49,13 +59,13 @@ class Plataforma extends Objetos{
     atualizar(){
         if(tecla_pressionada.KeyA === true ){
             if(this.x > 0){
-                plataforma.andar(-10);
+                this.andar(this.#andarEsquerda);
             }else{
                 this.#velocidade = 0
             }
         }else if(tecla_pressionada.KeyD == true){
             if(this.x + 100 < canvas.width){
-                plataforma.andar(10);
+                this.andar(this.#andarDireita);
             }else{
                 this.#velocidade = 0;
             }
@@ -63,17 +73,70 @@ class Plataforma extends Objetos{
         }else{
             this.#velocidade = 0;
         }
+        if(game_over === true){
+            this.#velocidade = 0;
+        }
         this.x += this.#velocidade;
-        
-    }
-    getVelocidade(){
-        return this.#velocidade;
     }
 }
 class Bola extends Objetos{
+    #velocidadeY
+    #velocidadeX
     constructor(x, y, largura, altura){
         super(x, y, largura, altura);
+        this.#velocidadeY = 3;
+        this.#velocidadeX = 3;
     }
+    atualizar(){
+        this.x -= this.#velocidadeX;
+        this.y -= this.#velocidadeY
+        this.colisaoHorizontal();
+        this.colisaoVertical();
+        this.colisaoPlataforma();
+        this.colisaoObstaculos();
+    }
+    colisaoHorizontal(){
+        if(this.x <= 0 || this.x + this.largura >= canvas.width){
+            this.#velocidadeX *= -1;
+        }
+    }
+    colisaoVertical(){
+        if(this.y <= 0){
+            this.#velocidadeY *= -1
+        }
+        if(this.y + this.altura >= canvas.height){
+            this.#velocidadeX = 0;
+            this.#velocidadeY = 0;
+            game_over = true;
+        }
+    }
+    colisaoPlataforma(){
+        if(
+            this.y + this.altura >= plataforma.y &&
+            this.x + this.largura >= plataforma.x &&
+            this.x <= plataforma.x + plataforma.largura
+        ){
+            this.#velocidadeY *= -1
+        }
+    }
+    colisaoObstaculos(){
+        if(
+            this.y + this.altura >= array_obstaculos.linha1.y &&
+            this.x + this.largura >= array_obstaculos.linha1.x &&
+            this.x <= array_obstaculos.linha1.x + array_obstaculos.linha1.largura &&
+            this.y <= array_obstaculos.linha1.y + array_obstaculos.linha1.altura
+        ){
+            this.#velocidadeY *= -1
+        }
+    }
+    //Implementação básica, será mexido completamente quando eu começar a mexer com os obstaculos,
+    //apenas feito para ter uma base da colisão e a função já estar iniciada.
+
+    //Existe um bug estranho que as vezes quando a bola bate no eixo X do obstaculo ela fica presa
+    //numa colisão sem parar até sair de cima do obstaculo, resolver.
+
+    //Aparemente o bug do eixo X está presente na colisão da plataforma também, averiguar o que está errado
+    //na condição if.
 }
 class Obstaculos extends Objetos{
     constructor(x, y, largura, altura){
@@ -94,6 +157,7 @@ function loop() {
     ctx.clearRect(0,0,canvas.width, canvas.height)
     array_obstaculos.linha1.desenha(ctx, 'yellow');
     bolinha.desenha(ctx, 'white');
+    bolinha.atualizar();
     plataforma.desenha(ctx,'red')
     plataforma.atualizar();
     desenhaPontuacao();
