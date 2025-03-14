4f2c54d3a63ff0ef343f2878a04c8f75bc05e7ab
João Carlos Formento <jcformento13@gmail.com>
2025-03-14 00:57:53 -0300

    Plataforma anda e para na parede

--- script/script.js
+++ script/script.js
@@ -1,6 +1,29 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+const tecla_pressionada = {"KeyA": false, "KeyD": false}
+
+document.addEventListener('keydown', (e) =>{
+    switch(e.code) {
+        case 'KeyA':
+            tecla_pressionada.KeyA = true;
+            break
+        case 'KeyD':
+            tecla_pressionada.KeyD = true;
+            break
+    }
+})
+document.addEventListener('keyup', (e) =>{
+    switch(e.code) {
+        case 'KeyA':
+            tecla_pressionada.KeyA = false;
+            break
+        case 'KeyD':
+            tecla_pressionada.KeyD = false;
+            break
+    }
+})
+
 class Objetos {
     constructor(x, y, largura, altura){
         this.x = x;
@@ -15,8 +38,36 @@ class Objetos {
 }
 
 class Plataforma extends Objetos{
+    #velocidade
     constructor(x, y, largura, altura){
         super(x, y, largura, altura);
+        this.#velocidade = 0;
+    }
+    andar(valorDirecao){
+        this.#velocidade = valorDirecao;
+    }
+    atualizar(){
+        if(tecla_pressionada.KeyA === true ){
+            if(this.x > 0){
+                plataforma.andar(-10);
+            }else{
+                this.#velocidade = 0
+            }
+        }else if(tecla_pressionada.KeyD == true){
+            if(this.x + 100 < canvas.width){
+                plataforma.andar(10);
+            }else{
+                this.#velocidade = 0;
+            }
+            
+        }else{
+            this.#velocidade = 0;
+        }
+        this.x += this.#velocidade;
+        
+    }
+    getVelocidade(){
+        return this.#velocidade;
     }
 }
 class Bola extends Objetos{
@@ -36,14 +87,15 @@ desenhaPontuacao = function(){
     ctx.fillText(`Pontos: 0`,30, 50)
 }
 const array_obstaculos = {linha1 : new Obstaculos(canvas.width/2 - 50, canvas.height - 600, 90, 40)}
-const plataforma = new Plataforma(canvas.width/2 - 60, canvas.height - 50, 100, 15)
-const bolinha = new Bola(canvas. width/2 - 20, canvas.height - 71, 20, 20)
+const plataforma = new Plataforma(canvas.width - 330, canvas.height - 50, 100, 15)
+const bolinha = new Bola(canvas. width - 290, canvas.height - 71, 20, 20)
 
 function loop() {
     ctx.clearRect(0,0,canvas.width, canvas.height)
     array_obstaculos.linha1.desenha(ctx, 'yellow');
     bolinha.desenha(ctx, 'white');
     plataforma.desenha(ctx,'red')
+    plataforma.atualizar();
     desenhaPontuacao();
     requestAnimationFrame(loop)
 }
